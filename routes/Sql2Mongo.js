e82bde56e2a4a49c74bb0ca272c193924360bb32
Mohammed Mehedi Hasan <u.urs.888@gmail.com>
2017-07-30 09:59:47 +0600

    mongo load
    mongo

--- routes/Sql2Mongo.js
+++ routes/Sql2Mongo.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var db= require('../dbconnect');
-
+var msisdn="01711085812";
 
 function loadFromMySql(){
-    var stmt="SELECT * FROM history";
-    db.query(stmt, function(err, res){
+    var stmt="SELECT * FROM history where msisdn=?";
+    db.query(stmt,msisdn, function(err, res){
         if(err)
             throw err;
         else 
