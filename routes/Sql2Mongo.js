1226bc9ca8f229e39def28adaba9b356a0df5781
Mohammed Mehedi Hasan <u.urs.888@gmail.com>
2017-07-30 10:41:26 +0600

    mongo
    mongo

--- routes/Sql2Mongo.js
+++ routes/Sql2Mongo.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db= require('../dbconnect');
+var MongoClient = require('mongodb').MongoClient
+var URL = 'mongodb://localhost:27017/cdr'
 var msisdn="01711085812";
 
 function loadFromMySql(){
@@ -17,6 +19,19 @@ function loadFromMySql(){
 
 }
 
-loadFromMySql();
+//loadFromMySql();
 
+
+
+MongoClient.connect(URL, function(err, db) {
+  if (err) return
+
+  var collection = db.collection('cdrHistory')
+  collection.insert({"msisdn": msisdn, "records":{"transaction_type":"VOICE","transaction_mode":"OUT","other_party_number":"01711085811","start_time":"2017-06-12 02:33:12","actual_consumption":"30","consumption_unit":"SEC","charges_in_bdt":"0.8"}}, function(err, result) {
+    collection.find().toArray(function(err, docs) {
+      console.log(docs[0])
+      db.close()
+    })
+  })
+})
 //module.exports = router;
