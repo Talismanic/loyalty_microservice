3d2f01118fe3c813f2fe4f474bd7b30b8c80cd19
Mohammed Mehedi Hasan <u.urs.888@gmail.com>
2017-07-30 11:05:58 +0600

    DB Connector
    DB Connector

--- routes/Sql2Mongo.js
+++ routes/Sql2Mongo.js
@@ -4,6 +4,8 @@ var db= require('../dbconnect');
 var MongoClient = require('mongodb').MongoClient
 var URL = 'mongodb://localhost:27017/cdr'
 var msisdn="01711085812";
+var mnDB=require('../mongoConnect');
+
 
 function loadFromMySql(){
     var stmt="SELECT * FROM history where msisdn=?";
@@ -22,20 +24,19 @@ function loadFromMySql(){
 //loadFromMySql();
 
 
-
-MongoClient.connect(URL, function(err, db) {
-  if (err){
-      console.log(err);
-      return
-
-  } 
-
-  var collection = db.collection('cdrHistory')
-  collection.insert({"msisdn": msisdn, "records":{"transaction_type":"VOICE","transaction_mode":"OUT","other_party_number":"01711085811","start_time":"2017-06-12 02:33:12","actual_consumption":"30","consumption_unit":"SEC","charges_in_bdt":"0.8"}}, function(err, result) {
-    collection.find().toArray(function(err, docs) {
-      console.log(docs[0])
-      db.close()
-    })
-  })
+mnDB.connect(URL, function(err) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  } else {
+      var collection = mnDB.get().collection('cdrHistory');
+      collection.find().toArray(function(err,docs){
+          console.log(docs[0]);
+          return
+      })
+ 
+  }
 })
+
+
 //module.exports = router;
